a1f7db3f7c5cc714985c06cfdd23e92b74195b31
asiradnan <asiradnan23@gmail.com>
2024-08-16 11:48:43 +0600

    Initialized

---
+++ app/src/main/java/com/asiradnan/muslimapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.asiradnan.muslimapp
+
+import android.icu.util.Calendar
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import java.text.SimpleDateFormat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val  now:Calendar = Calendar.getInstance();
+        val formatteddate = SimpleDateFormat("dd MMMM").format(now.time);
+        val datedisplay:TextView = findViewById(R.id.datedisplay)
+        datedisplay.text = formatteddate;
+    }
+}
